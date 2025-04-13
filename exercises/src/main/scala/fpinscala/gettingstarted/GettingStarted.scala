f5aa68aedb6d31579bb0f2325b5fa2d6fef96403
taemath <hirotaka081630@gmail.com>
2025-04-13 15:42:57 +0900

    feat(p.34) curry & compose

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -174,18 +174,19 @@ object PolymorphicFunctions {
   def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
-  // Exercise 3: Implement `curry`.
-
+  // Exercise 2.3: Implement `curry`.
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = { a => b =>
+    f(a, b)
+  }
 
   // NB: The `Function2` trait has a `curried` method already
 
-  // Exercise 4: Implement `uncurry`
-  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
-    ???
+  // Exercise 2.4: Implement `uncurry`
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = { (a, b) =>
+    f(a)(b)
+  }
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -199,6 +200,7 @@ object PolymorphicFunctions {
 
   // Exercise 5: Implement `compose`
 
-  def compose[A, B, C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C = { a =>
+    f(g(a))
+  }
 }
